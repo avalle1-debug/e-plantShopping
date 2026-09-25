88c8fa1145ac8e6fc33429ce841c184653f4eb52
Arturo Valle <avalle1@charlotte.edu>
2026-09-25 08:50:41 -0400

    Integrated Redux functionality in components

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,9 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
+    const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -264,6 +267,8 @@ function ProductList({ onHomeClick }) {
     [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
   }));
 };
+
+
 
     return (
         <div>
